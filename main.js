e82bbba72839dc4a63042f2614023d5a81c96860
rhonamackay <rhonamackay@outlook.com>
2022-12-03 18:18:49 +0000

    Version 2.0

--- main.js
+++ main.js
@@ -2,25 +2,39 @@ let question;
 let correctAnswer;
 let wrongAnswers;
 let score = 0;
-let url = 'https://opentdb.com/api.php?amount=1&type=multiple&difficulty=easy&category=27'
+let sessionToken = '';
+let url = ''
+let gotItRight = false;
+
+
+fetchToken()
 
 //Selecting elements and adding event listeners
-document.querySelector("#new-question").addEventListener('click', fetchQuestion);
+document.querySelector("#check-answer").addEventListener('click', checkAnswer);
 const changeDif = document.querySelector('#difficulty');
 const changeCat = document.querySelector('#category');
 const radiobuttons = document.querySelectorAll(".radio-button");
 const scoreElement = document.querySelector('#score');
 const resultElement = document.querySelector('#result')
 for (let i = 0; i < radiobuttons.length; i++) {
-    radiobuttons[i].addEventListener('click', checkAnswer);
+    radiobuttons[i].addEventListener('click', collectAnswer);
 }
 const dropdowns = document.querySelectorAll('.dropdown')
 for (let i=0; i < dropdowns.length; i++) {
     dropdowns[i].addEventListener('change', changeURL);
 }
 
+async function fetchToken() {
+    let response = await fetch('https://opentdb.com/api_token.php?command=request');
+    let tokenData = await response.json();
+    console.log(tokenData)
+    sessionToken = tokenData.token
+    url='https://opentdb.com/api.php?amount=1&type=multiple&difficulty=easy&category=27&token=' + sessionToken
+    fetchQuestion();
+}
+
 function changeURL() {
-    let newURL = ['https://opentdb.com/api.php?amount=1&type=multiple&','difficulty=easy','&category=27']
+    let newURL = ['https://opentdb.com/api.php?amount=1&type=multiple&','difficulty=easy','&category=27', '&token=', sessionToken]
     newURL[1] = `difficulty=${changeDif.value}`
     newURL[2] = `&category=${changeCat.value}`
     url = newURL.toString().replaceAll(',', '') 
@@ -28,15 +42,20 @@ function changeURL() {
 }
 
 async function fetchQuestion() {
+    console.log(url)
+    gotItRight = false
     resultElement.classList.remove("fade-in-out")
     let buttons = document.querySelectorAll(".radio-button")
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].checked = false;
         }
-    console.log(url)
     let response = await fetch(url);
     let data = await response.json();
-    renderQuestion(data)
+    if (data.response_code === 4) {
+        alert('You have recieved all possible questions! Either chose a different difficulty and/or topic, or reload to reset your session')
+    } else { 
+        renderQuestion(data)
+    }
 }
 
 async function renderQuestion(data) {
@@ -54,32 +73,41 @@ async function renderQuestion(data) {
     document.querySelector("#label-1").textContent = answers[1];
     document.querySelector("#label-2").textContent = answers[2];
     document.querySelector("#label-3").textContent = answers[3];
-    scoreElement.textContent = `Your score: ${score}`
+    scoreElement.textContent = `Your score: ${score}`;
 }
 
-fetchQuestion();
-
-function checkAnswer(event) {
+function collectAnswer(event) {
     if (event.target.nextSibling.textContent === correctAnswer) {
+        gotItRight = true;
+    } else {
+        gotItRight = false;
+    }
+}
+
+function checkAnswer() {
+    if (resultElement.classList.contains("fade-in-out") === true) {
+        resultElement.classList.remove("fade-in-out");
+    }
+    if (gotItRight === true) {
         wellDone();
+        gotItRight = false;
     } else {
         tooBad();
     }
 }
 
-function wellDone() {
+async function wellDone() {
     score++ ;
     scoreElement.textContent = `Your score: ${score}`;
-    resultElement.textContent = 'Well done, correct answer!';
+    resultElement.textContent = 'Well done, correct answer! 🎉';
     resultElement.classList.toggle("fade-in-out");
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    fetchQuestion();
 }
 
 function tooBad() {
-    score-- ;
-    scoreElement.textContent = `Your score: ${score}`;
-    resultElement.textContent = 'Wrong answer, too bad :(';
+    resultElement.textContent = 'Wrong answer 😔';
     resultElement.classList.toggle("fade-in-out");
-
 }
 
 //HTML entities decoder
--- readme.md
+++ readme.md
@@ -1,1 +1,20 @@
-A simple trivia game using fetch API
+A simple trivia game using fetch API, Javascript, HTML and CSS
+
+# Expansion ideas
+- implementing leaderboard using a database
+- auhtentication/authorisation so users can log in to save their scores
+
+
+## Done 💯
+# Version 2.0
+✅ Fix bug where clicking repeatedly on the same answer will keep on increasing/decreasing score
+    DONE: Now answer is checked when 'Check answer' button is clicked. A new question is generated only when the selected answer is correct
+✅ Make design more accessible: back-up fonts, larger text size
+✅ Look into avoiding repeat questions
+    DONE: I looked into the Open Trivia DB API docs in more details and found the option to generate a token to identify the session. The token is requested on page load and the user will not get repeat questions while using the same token. Alert shows if they have answered all possible questions for topic and difficulty level.
+### Version 1.0:
+✅ finesse fetch URL
+    ✅ make sure changing difficulty doesn't stack but adds
+    ✅ change difficulty and category at different times without messing each other up - maybe have a single function ?
+✅ scoring system - maybe at the bottom of the question box?
+✅ Use CSS variables in style sheet
